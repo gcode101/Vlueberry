326e5bf07957d433c0d7f66444e27d26132b8488
gcode101 <gcode101@users.noreply.github.com>
2025-01-20 12:39:02 -0500

    added a toggle sub header in the bookmarks page

---
+++ Project_Code/frontend/src/routes/Bookmarks/Bookmarks.styles.jsx
@@ -0,0 +1,9 @@
+import { styled } from "@mui/system";
+
+export const SubHeader = styled("div")`
+    display: flex;
+    width: 20%;
+    align-self: center;
+    justify-content: space-evenly;
+    border-bottom: 1px solid white;
+`
